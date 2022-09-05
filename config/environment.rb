e3e0b49091003c45b0e29fe5887e2d40b61462e4
achyut kadam <achyut.k@acldigital.com>
2022-09-05 11:51:58 +0530

    Create staging environments

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the Rails application.
 require_relative "application"
+puts "++++++++aaaa+++++"
+p Rails.env
+puts "+++++++++++++"
+ENV['RAILS_ENV'] = 'staging'
+puts ENV['RAILS_ENV']
+puts "+++++++++++++"
 
 # Initialize the Rails application.
 Rails.application.initialize!
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_28_090114) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_03_110435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_28_090114) do
     t.string "contact"
     t.integer "gender"
     t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
